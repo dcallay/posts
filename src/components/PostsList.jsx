d763d8c64d86306065ee46e69994a0ba0209e720
datifull <david@callay.com>
2025-08-13 23:58:56 +0200

    Refactor NewPost and PostsList components to manage post data and submission handling

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,17 +5,6 @@ import Post from "./Post";
 import classes from "./PostsList.module.css"
 
 function PostList({isPosting, onStopPosting}) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
     // let modalContent;
 
     // if (modalIsVisible) {
@@ -28,20 +17,38 @@ function PostList({isPosting, onStopPosting}) {
     //     );
     // }
 
+    const [posts, setPosts] = useState([]);
+
+    function addPostsHandler(postData) {
+        // if you use a previous state to set the new state then a function needs to be used
+        // setPosts([postData, ...posts]);
+        // react makes sure to update the correct state even if there are multiple pending states
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
+
+    // maps takes a function, receiving that item and return value should be mapped
+
     return ( 
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onBodyChange={bodyChangeHandler}
-                        onAuthorChange={authorChangeHandler}
+                        onCancel={onStopPosting}
+                        onAddPost={addPostsHandler}
                     />
                 </Modal>                
             )}
-            <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
-                <Post author="Liz" body="Preciosirri" />
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                </ul>                
+            )}
+            {posts.length === 0 && 
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            }
         </>
     );
 }
